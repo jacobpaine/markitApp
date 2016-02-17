e842f1498c8dc00b785744a4e1ba61880523b19e
jacobpaine <painejacob@gmail.com>
2016-02-17 09:26:03 -0600

    Added some comments to controllers, partials, and models

--- controllers/quote.js
+++ controllers/quote.js
@@ -4,31 +4,40 @@ const request = require('request');
 // stockquote is the model upon which data is organized (?)
 const Stock = require('../models/stockquote');
 
-// From the route, this index method renders quote.jade
+// From the route (api.js), this index method renders quote.jade
 module.exports.index = (req, res) => {
   res.render('quote');
 };
 
 module.exports.findstock = (req, res) => {
+// From jade, the req is pulling whatever was placed in the input field (quote.jade line 10).
   const symbol = req.body.sym;
+// Find correct API URL to get the correct JSON. Read the docs.
   const url = `http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=${symbol}`;
   request.get(url, (err, response, body) => {
     if (err) throw err;
-
+// Parse the body into a JSON to be used later objects.
     const thing = JSON.parse(body);
-
+// Oh look! An object! That is pulling Name, Symbol and LastPrice from the API and sticking it into the model as name, symbol, price.
     const myStock = new Stock({
       name: thing.Name,
       symbol: thing.Symbol,
       price: thing.LastPrice
     });
-
-    console.log(myStock);
+//Logs to check functionality.
     const stockName = thing.Name;
     console.log(stockName);
+    console.log(myStock);
+//myStock gets sent to quotedetails.jade (lines 10-16)
+    res.render('quotedetails', myStock);
 
-    res.render('quotedetails', {
-      sym: thing.Name
-    });
+// Useful Information: Not being used.
+// Render puts the info onto quotedetails.jade, putting Name, Symbol and LastPrice into the jade using the matching name (e.g. name, symbol, price)
+    // res.render('quotedetails', {
+    //   name: thing.Name,
+    //   symbol: thing.Symbol,
+    //   price: thing.LastPrice
+    // });
+
   });
 };
--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/quote');
-
+// Although the name of this file is api.js, it is mostly unimportant. It is /quote that we need to find in the controller. The name 'api.js' is used in routes/index.js specifically for the router. MODULARITY!
 router.get(`/quote`, ctrl.index);
 
 router.post(`/quote`, ctrl.findstock);
